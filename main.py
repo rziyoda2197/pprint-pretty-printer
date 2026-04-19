fec2485063ad62452a75653c8d02f9e9b4259063
rziyoda2197 <rziyoda2197@gmail.com>
2026-04-19 20:15:48 +0500

    🚀 Add main application code

---
+++ main.py
@@ -0,0 +1,15 @@
+import pprint
+
+d = {
+    "name": "John",
+    "age": 30,
+    "city": "New York",
+    "hobbies": ["reading", "swimming", "cycling"],
+    "address": {
+        "street": "123 Main St",
+        "apartment": 4,
+        "country": "USA"
+    }
+}
+
+pprint.pprint(d)
